e65279b591a74a4e7c21a1fd6ea26cfab2c93360
Josh Thomas <jthoms1@gmail.com>
2025-10-29 16:16:04 -0500

    update to remove unused files, and correct import root

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "lib/main.js"),
+      entry: path.resolve(__dirname, "lib/index.ts"),
       name: "MyLib",
       formats: ["es", "umd"],
       fileName: (format: string) => `sample-design-system.${format}.js`,
